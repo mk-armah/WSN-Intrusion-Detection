5148dd7ce27b717fca4e8c86083b192219307082
mk-armah <mikeyarmah@gmail.com>
2022-11-21 01:21:20 +0000

    fix predict path

--- app/main.py
+++ app/main.py
@@ -4,19 +4,12 @@ import joblib
 from utils import load_model
 from aws import AWS
 import os
-
-#load environment variables
-from dotenv import load_dotenv
-load_dotenv(".env")
-
-#assign environment variables to variables
-ACCESS_KEY_ID = os.environ.get("ACCESS_KEY_ID")
-SECRET_ACCESS_KEY = os.environ.get("SECRET_ACCESS_KEY")
+import numpy as np
 
 app  = FastAPI()
-s3 = AWS(ACCESS_KEY_ID,SECRET_ACCESS_KEY,"wsn-intrusion-detection")
 
-@app.post("/{}/predict")
+
+@app.post("/{model}/predict")
 def predictions( is_ch: int, dist_to_ch: int|float,
                 adv_s: int, adv_r: int,
                 join_s: int, join_r: int,
@@ -26,21 +19,31 @@ def predictions( is_ch: int, dist_to_ch: int|float,
                 dist_ch_to_bs:int|float,send_code: int,
                 expaned_energy:int|float, model:Model):
                 
-            
-            features = [is_ch,dist_to_ch,adv_s,adv_r,join_s,
-            join_r,sch_s,sch_r,rank,data_s,data_r,data_sent_to_bs,
-            dist_ch_to_bs,send_code,expaned_energy]
+        
+            features = [
+                        is_ch,dist_to_ch,adv_s,                 
+                        adv_r,join_s,join_r,sch_s,sch_r,                
+                        rank,data_s,data_r,data_sent_to_bs,     
+                        dist_ch_to_bs,send_code,expaned_energy 
+                        ]
             
             #load model
-            model = load_model(model)
-            output = model.predict(features)
-            
-            return {"Features":features,"Model Prediction":output}
+
+            try:
+                modelpath = "./models/"+model.value+".chael"
+                model = joblib.load(modelpath)
+
+            except Exception as exc:
+                return {'status':"model unavailable"}
+
+            prediction = model.predict(np.array(features).reshape(1,-1))  
+            prediction = list(prediction)
+
+            return {"status":prediction}
+
 
 
 @app.get("/{}/download")
 def getmodel():
     """download/send model to user"""
     return {}
-
-
