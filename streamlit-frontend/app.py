9e36a0b40feb7691679bedb7f69b1ed82fef94d6
mk-armah <mikeyarmah@gmail.com>
2022-11-22 00:29:40 +0000

    ready : 22-11-22

--- streamlit-frontend/app.py
+++ streamlit-frontend/app.py
@@ -1,110 +1,176 @@
 import pandas as pd
 import streamlit as st
+from utils import predict
+from ast import literal_eval
+
+hide_menu = """<style>#MainMenu {
+    visibility:visible
+}
+    footer{
+        visibility: visible;
+    }
+
+    footer:after{
+        visibility: visible;
+        content = 'copyright chael.ai';
+        display:block
+        position:relative;
+        color:tomato;
+    }
+}</style>"""
+
+[theme]
+base="dark"
+
+def main():
+
+    st.set_page_config(page_title="WSN ID", page_icon=":}", layout="centered")
+    st.title("WSN Intrusion Detection")
+
+    #hide menu bar
+    st.markdown(hide_menu,unsafe_allow_html = True)
 
 
-st.set_page_config(page_title="WSN ID", page_icon=":}", layout="centered")
+    model = st.sidebar.selectbox(
+            "Select Classifier:", ['Adaboost',"Random Forest", "Decision Tree", "Artificial Neural Network"], index=1
+        )
 
-st.title("WSN Intrusion Detection")
+    cols = st.sidebar.columns(2)
 
-import time
+    #temporal feature | only adaboost is currently in production
+    if model == 'Adaboost':
 
-model = st.sidebar.selectbox(
-        "Select Classifier:", ['Adaboost',"Random Forest", "Decision Tree", "Artificial Neural Network"], index=1
+        cols[0].download_button(
+            label="Download Model",
+            data="csv",
+            file_name='adaboost.chael',
+            mime='text/csv',
+        )
+
+        cols[1].download_button(
+        label="Source Code",
+        data="csv",
+        file_name='source.code',
+        mime='text/csv',
     )
 
-cols = st.sidebar.columns(2)
-cols[0].download_button(
-    label="Download Model",
-    data="csv",
-    file_name='large_df.csv',
-    mime='text/csv',
-)
-
-cols[1].download_button(
-    label="Download Data",
-    data="csv",
-    file_name='dataset.csv',
-    mime='text/csv',
-)
-
-#Read me
-
-# with open("README.text",'r') as file:
-#     readme = file.read()
-
-# st.sidebar.write(readme)
-
-st.sidebar.write(
-    f"[Github Repository](https://github.com/mk-armah/WSN-Intrusion-Detection)"
-)
-st.sidebar.write(
-    f"Engineered By: Chael.AI"
-)
-
-st.image('https://psiborg.in/wp-content/uploads/2021/07/Embedded-banner-2048_11zon-1.jpg')
-form = st.form(key="annotation")
+    else:
+        st.sidebar.write("Model is currently unavailable")
 
 
-with form:
-    cols = st.columns((1, 1))
-    dist_to_ch = cols[0].number_input("dist_to_ch")
-    is_channel = cols[1].selectbox(
-        "is channel:", [0,1], index=1
-    )
 
-    cols = st.columns(3)
-    adv_s = cols[0].number_input("adv_s") 
-    adv_r = cols[1].number_input("adv_r")
-    expaned_energy = cols[2].number_input("expaned_energy")
 
-    cols = st.columns(3)
-    sch_s = cols[0].number_input("sch_s") 
-    sch_r = cols[1].selectbox(
-        "sch_r:", [0,1], index=1
-    )
-    send_code = cols[2].slider("send_code",0, 15,1)
+    #Read me
 
-    cols = st.columns(2)
-    data_sent_to_bs = cols[0].number_input("data_sent_to_bs") 
-    dist_ch_to_bs = cols[1].number_input("dist_ch_to_bs")
+    # with open("README.text",'r') as file:
+    #     readme = file.read()
 
-    cols = st.columns(2)
-    data_s = cols[0].number_input("data_s") 
-    data_r = cols[1].number_input("data_r")
-    
-    cols = st.columns(3)
-    join_s = cols[0].number_input("join_s") 
-    join_r = cols[1].number_input("join_r")
-    rank = cols[2].number_input("rank")
+    # st.sidebar.write(readme)
 
-    submitted = st.form_submit_button(label="Predict")
+    # st.sidebar.write(
+    #     f"[Github Repository](https://github.com/mk-armah/WSN-Intrusion-Detection)"
+    # )
+    # st.sidebar.write(
+    #     f"Engineered By: Chael.AI"
+    # )
 
-if submitted:
-    with st.spinner(text='In progress'):
-        import time
-        time.sleep(2)
-        st.write(data_r)
-        st.success("{} Attack Detected".format("predictions")) #predictions goes
-        st.balloons() 
-        st.info("Don't forget to leave a comment below if you like this work :)")
+    st.image('https://psiborg.in/wp-content/uploads/2021/07/Embedded-banner-2048_11zon-1.jpg')
+    form = st.form(key="annotation")
 
-        expander = st.expander("Leave us a comment")
-        with expander:
-            comment_form = st.form(key="comments")
-            with comment_form:
-                cols = st.columns((2))
-                name = cols[0].text_input("name")
-                email = cols[1].text_input("email address")
-                comment = st.text_area("Comment:")
 
-                submit_comment = st.form_submit_button(label="Submit")
+    with form:
+        cols = st.columns((1, 1))
+        dist_to_ch = cols[0].number_input("dist_to_ch")
+        is_channel = cols[1].selectbox(
+            "is channel:", [0,1], index=1
+        )
 
-                if submit_comment:
-                    st.info("Hello {}, your comments on this work has been recieved, out team will contact you via email as soon as possible, Thanks :) ")
-                    st.write(name)
+        cols = st.columns(3)
+        adv_s = cols[0].number_input("adv_s") 
+        adv_r = cols[1].number_input("adv_r")
+        expaned_energy = cols[2].number_input("expaned_energy")
 
-# [is_channel, dist_to_ch,adv_s,adv_r,join_s,join_r,
-# sch_s,sch_r,rank,data_s,data_r,data_sent_to_bs,
-# dist_ch_to_bs,send_code,expaned_energy]        
+        cols = st.columns(3)
+        sch_s = cols[0].number_input("sch_s") 
+        sch_r = cols[1].selectbox(
+            "sch_r:", [0,1], index=1
+        )
+        send_code = cols[2].slider("send_code",0, 15,1)
 
+        cols = st.columns(2)
+        data_sent_to_bs = cols[0].number_input("data sent to base station")
+        dist_ch_to_bs = cols[1].number_input("distance from cluster head to base station")
+
+        cols = st.columns(2)
+        data_s = cols[0].number_input("data_s") 
+        data_r = cols[1].number_input("data_r")
         
+        cols = st.columns(3)
+        join_s = cols[0].number_input("join_s") 
+        join_r = cols[1].number_input("join_r")
+        rank = cols[2].number_input("rank")
+
+        submitted = st.form_submit_button(label="Predict")
+
+    if submitted:
+        
+        with st.spinner(text='In progress'):
+
+            prediction = predict(model = model,is_ch = is_channel,dist_to_ch = float(dist_to_ch),
+                                adv_s = int(adv_s),adv_r = int(adv_r),join_s = int(join_s),
+                                join_r = int(join_r),sch_s = int(sch_s), sch_r = int(sch_r),
+                                rank = 1,data_s = int(data_s), data_r = int(data_r),
+                                data_sent_to_bs = int(data_sent_to_bs),
+                                dist_ch_to_bs = float(dist_ch_to_bs),
+                                send_code = send_code, expaned_energy = float(expaned_energy))
+
+            try:
+                
+                assert prediction.status_code == 200  #upon successful request/prediction
+                
+                predictions_in_bytes = prediction.content
+                prediction = literal_eval(predictions_in_bytes.decode('utf-8'))
+
+                prediction = prediction['status'][0]
+
+                if prediction == 'Normal':
+
+                    st.success("{} Attack  |  All clear ".format(prediction)) #predictions goes
+                    st.balloons() 
+            
+                elif prediction in ['TDMA',"Flooding","Blackhole","Grayhole"]:
+
+                    st.warning("{} Attack Detected".format(prediction)) #predictions goes
+            
+            except AssertionError:
+                if prediction.status_code == 404:
+
+                    error = f"{model} Model Not Found | current version only supports adaboost please consider switching from {model} to adaboost"
+                    st.warning(error)
+                    
+                else:
+
+                    st.warning("An unknown error occured, please consider checking your inputs")
+
+            finally:
+                st.info("Don't forget to leave a comment below if you like this work :)")
+
+
+            expander = st.expander("Leave us a comment")
+            with expander:
+                comment_form = st.form(key="comments")
+                with comment_form:
+                    cols = st.columns((2))
+                    name = cols[0].text_input("name")
+                    email = cols[1].text_input("email address")
+                    comment = st.text_area("Comment:")
+
+                    submit_comment = st.form_submit_button(label="Submit")
+
+                    if submit_comment:
+                        st.info("Hello {}, your comments on this work has been recieved, out team will contact you via email as soon as possible, Thanks :) ")
+                        st.write(name)
+    
+
+if __name__ == '__main__':
+    main()
--- streamlit-frontend/utils.py
+++ streamlit-frontend/utils.py
@@ -1,9 +1,10 @@
 import requests
 
-def predict(**kwargs):
+
+
+def predict(model,**kwargs):
 
     #fifteen features :)
-    url = "http://wsn-api.herokuapp.com/adaboost/predict?is_ch={}&dist_to_ch={}&adv_s={}&adv_r={}&join_s={}&join_r={}&sch_s={}&sch_r={}&rank={}&data_s={}&data_r={}&data_sent_to_bs={}&dist_ch_to_bs={}&send_code={}&expaned_energy={}"
 
     try:
         # assert len(kwargs.values()) == 15
@@ -20,30 +21,20 @@ def predict(**kwargs):
     except AssertionError as ass:
         print(check)
         print("an input is feature was excluded {}".format(ass))
+                 
 
-    inputs = url.format(kwargs['is_ch'],kwargs['dist_to_ch'],
-                        kwargs['adv_s'],kwargs['adv_r'],kwargs['join_s'],
-                        kwargs['join_r'],kwargs['sch_s'],kwargs['sch_r'],kwargs['rank'],
-                        kwargs['data_s'],kwargs['data_r'],kwargs['data_sent_to_bs'],
-                        kwargs['dist_ch_to_bs'],kwargs['send_code'],kwargs['expaned_energy'])                 
-
-    values = tuple(kwargs.values())      #get values from the dict and convert dict object to tuple
+    url = "http://wsn-api.herokuapp.com/adaboost/predict?"
+    output = requests.get(url= url,params = kwargs)
     
-    request_url = url.format(*values)           #unpack tuple
-    
-    print(request_url)
+    return output
 
-    prediction = requests.post(requests)
-    print(request_url)
-
-    #return prediction
 
 if __name__ == "__main__":
 
-    prediction = predict(is_ch = 1,dist_to_ch = 1,adv_s = 1,adv_r = 1,
+    predictions = predict(model = 'adaboost',is_ch = 1,dist_to_ch = 1,adv_s = 1,adv_r = 1,
                 join_s = 1,join_r = 1,sch_s =1,sch_r = 1,rank = 1,
                 data_s = 1,data_r = 1,data_sent_to_bs = 1,
                 dist_ch_to_bs = 1,send_code = 1,expaned_energy = 1)
+
     
-    print(prediction)
-    
+    print(predictions.content)
